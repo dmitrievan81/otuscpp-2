a12f72436b79c85e3fb4a7e90bf31dc8d385b9d1
Anton Dmitriev <antondmit@gmail.com>
2022-10-06 22:21:34 +0300

    otus - home work 2
    Вынести функцию move_me_out наружу в отдельный файл

--- example.cpp
+++ example.cpp
@@ -1,7 +1,10 @@
 #include "example.h"
-
 #include <iostream>
 
 void example_function() {
 	std::cout << "Hello from example function!" << std::endl;
+}
+
+void todo_move_me_out() {
+	std::cout << "This function should be located in the separated source file" << std::endl;
 }
--- example.h
+++ example.h
@@ -1,3 +1,4 @@
 #pragma once
 
 void example_function();
+void todo_move_me_out();
